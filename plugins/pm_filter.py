59148e874c11aff710e0df9149205ab579732711
AFxSU <109891896+AFxSU@users.noreply.github.com>
2022-08-19 22:50:05 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -697,7 +697,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             InlineKeyboardButton("➕️ 𝙰𝙳𝙳 𝙼𝙴 𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 ➕️", url=f"http://t.me/{temp.U_NAME}?startgroup=true")
             ],[
             InlineKeyboardButton("🔍 𝚂𝙴𝙰𝚁𝙲𝙷 🔍", switch_inline_query_current_chat=''), 
-            InlineKeyboardButton("🛑 𝚄𝙿𝙳𝙰𝚃𝙴𝚂 🛑", url="https://t.me/at3botsupport")
+            InlineKeyboardButton("⭕️ 𝚄𝙿𝙳𝙰𝚃𝙴𝚂 ⭕️", url="https://t.me/at3botsupport")
             ],[      
             InlineKeyboardButton("💠 𝙷𝙴𝙻𝙿 💠", callback_data="help"),
             InlineKeyboardButton("🍃 𝙰𝙱𝙾𝚄𝚃 🍃 ", callback_data="about")
@@ -820,7 +820,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         )
     elif query.data == "about":
         buttons= [[
-            InlineKeyboardButton('❣️ 𝚂𝙾𝚄𝚁𝙲𝙴 𝙲𝙾𝙳𝙴 ❣️', callback_data='source')
+            InlineKeyboardButton('🍁 𝚂𝙾𝚄𝚁𝙲𝙴 𝙲𝙾𝙳𝙴 🍁', callback_data='source')
             ],[
             InlineKeyboardButton('🏠 𝙷𝙾𝙼𝙴 🏠', callback_data='start'),
             InlineKeyboardButton('🔐 𝙲𝙻𝙾𝚂𝙴 🔐', callback_data='close_data')
